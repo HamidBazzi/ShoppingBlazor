856144b55f223dc621570a0b73a37ddeed7464e3
Hamid Bazzi <bazzi.hamid@gmail.com>
2025-03-25 23:59:41 +0330

    create category ui

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using ShoppingBlazor.Components;
 using ShoppingBlazor.Components.Account;
 using ShoppingBlazor.Data;
+using ShoppingBlazor.Repository;
+using ShoppingBlazor.Repository.IRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
