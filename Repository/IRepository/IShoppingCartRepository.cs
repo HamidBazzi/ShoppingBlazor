14fceb6d058f56150a680df0072224ffd4f688e1
Hamid Bazzi <bazzi.hamid@gmail.com>
2025-04-07 01:15:49 +0330

    Build Shopping Cart Logic

---
+++ Repository/IRepository/IShoppingCartRepository.cs
@@ -0,0 +1,11 @@
+﻿using ShoppingBlazor.Data;
+
+namespace ShoppingBlazor.Repository.IRepository
+{
+    public interface IShoppingCartRepository
+    {
+        public Task<bool> UpdateCartAsync(string userId, int productId, int updateBy);
+        public Task<IEnumerable<ShoppingCart>> GetAllAsync(string? userId);
+        public Task<bool> ClearCartAsync(string? userId);
+    }
+}
