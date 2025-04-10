58de6a72018924a4fd54f03dfa7f431a53641a06
Hamid Bazzi <bazzi.hamid@gmail.com>
2025-04-10 12:56:39 +0330

    Create Database for OrderHeader and OrderDetail

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -8,6 +8,8 @@ namespace ShoppingBlazor.Data
         public DbSet<Category> Category { get; set; }
         public DbSet<Product> Product { get; set; }
         public DbSet<ShoppingCart> ShoppingCart { get; set; }
+        public DbSet<OrderHeader> OrderHeader { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
